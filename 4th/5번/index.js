e92e2b2ba0aa162c047af465f9a0ba2b7cb9b472
hosu kang <rkdghtn6036@gmail.com>
2023-11-15 16:59:38 +0900

    4회차 과제 완료

--- 4th/5번/index.js
+++ 4th/5번/index.js
@@ -1,3 +1,29 @@
 // 여기에 1번 문제의 답을 작성하세요.
+async function fetchData() {
+  try {
+    let response = await fetch("https://dummyjson.com/products");
+    let data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
 
 // 여기에 2번 문제의 답을 작성하세요.
+async function postData() {
+  try {
+    let response = await fetch("https://dummyjson.com/products/add", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        title: "BMW Pencil",
+      }),
+    });
+    let data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+postData();
